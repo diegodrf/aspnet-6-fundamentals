d4ac89193fbae94c8402bc9b4da7dff7102aa735
Diego Faria <diego.rdfaria@gmail.com>
2022-10-17 14:24:09 -0300

    Adicionar arquivos de projeto.

---
+++ Todo/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+namespace Todo.Data
+{
+    public class AppDbContext:DbContext
+    {
+        DbSet<TodoModel> Todos => Set<TodoModel>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=todoapp.db;Cache=Shared");
+        }
+    }
+}
